afee356f7197f3637e6480cd940d883a3f776aa3
praise <daramolapraise@gmail.com>
2021-12-16 12:23:40 -0600

    added replace folder name

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,42 @@ use std::str;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() > 3 {
-        let directory: Vec<&str> = (&args[1]).split(":").collect();
+        let mut directory: Vec<&str> = (&args[1]).split(":").collect();
         let placeholder = &args[2];
         let replacement = &args[3];
-        let path: String = format!("/{}", String::from(directory[1..directory.len()].join("/")));
+        let mut result = 0;
+        let mut path: String = format!(
+            "/{}",
+            String::from(&directory[1..directory.len()].join("/"))
+        );
+        println!("Folder: {}\n\n", path);
         if Path::new(&path).exists() {
             println!(
-                "Changing: \n\n---------\n\nFrom: {}\n\nTo: {}\n\n In: {}\n\n---------\n\n",
+                "Changing: \n\n---------\n\nFrom: {}\n\nTo: {}\n\nIn: {}\n\n---------\n\n",
                 placeholder, replacement, &path,
             );
             if Path::new(&path).is_dir() {
+                let last_dir = &directory.len() - 2;
+                if &directory[last_dir] == placeholder {
+                    directory[last_dir] = &replacement;
+                    let replaced_path = format!(
+                        "/{}",
+                        String::from(&directory[1..directory.len()].join("/"))
+                    );
+                    fs::rename(
+                        &path[..&path.len() - 1],
+                        &replaced_path[..&replaced_path.len() - 1],
+                    )
+                    .expect(&path);
+                    path = format!("{}/", &replaced_path);
+                    result = 1;
+                }
                 let paths = fs::read_dir(&path).unwrap();
-                let result = recursive_print(paths, &placeholder, &replacement);
-                println!("Occurrences changed: {}", result);
+                result += recursive_print(paths, &placeholder, &replacement);
             } else if Path::new(&path).is_file() {
                 replace(&path, &placeholder, &replacement, 0);
             }
+            println!("Occurrences changed: {}", result);
         }
     }
 }
